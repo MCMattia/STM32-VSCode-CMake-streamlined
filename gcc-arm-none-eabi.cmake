b8bd9907137b5796d5f2e23dddc2c7f165b46399
MCMattia <mcemail@posteo.ch>
2024-05-01 14:38:50 +0200

    Initial commit

---
+++ gcc-arm-none-eabi.cmake
@@ -0,0 +1,18 @@
+message("Entering ${CMAKE_CURRENT_LIST_FILE}")
+
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+set(CMAKE_SYSTEM_NAME "Generic")
+
+# Skip link step during toolchain validation.
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(TOOLCHAIN_PREFIX   "/opt/st/stm32cubeide_1.15.0/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.12.3.rel1.linux64_1.0.100.202403111256/tools/bin/arm-none-eabi-")
+set(CMAKE_C_COMPILER   "${TOOLCHAIN_PREFIX}gcc")
+set(CMAKE_ASM_COMPILER "${TOOLCHAIN_PREFIX}gcc")
+set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++")
+set(CMAKE_AR           "${TOOLCHAIN_PREFIX}ar")
+set(CMAKE_LINKER       "${TOOLCHAIN_PREFIX}ld")
+set(CMAKE_OBJCOPY      "${TOOLCHAIN_PREFIX}objcopy")
+set(CMAKE_RANLIB       "${TOOLCHAIN_PREFIX}ranlib")
+set(CMAKE_SIZE         "${TOOLCHAIN_PREFIX}size")
+set(CMAKE_STRIP        "${TOOLCHAIN_PREFIX}ld")
