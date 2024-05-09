f975b181251d5e20f0b9ca0a760cac6174090256
MCMattia <mcemail@posteo.ch>
2024-05-09 18:30:54 +0200

    feat: migrate from STM32L4A6AGIX to  STM32L4A6ZGTX

--- Application/main.cpp
+++ Application/main.cpp
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "stm32l4xx_hal.h"
+#include "gpio.h"
 
 extern "C" {
 extern void SystemClock_Config();
@@ -10,4 +10,14 @@ int main()
     HAL_Init();
 
     SystemClock_Config();
+
+    MX_GPIO_Init();
+
+    while(true)
+    {
+   	    HAL_GPIO_WritePin(LD2_GPIO_Port,LD2_Pin, GPIO_PIN_SET);
+        HAL_Delay(1000);
+        HAL_GPIO_WritePin(LD2_GPIO_Port,LD2_Pin, GPIO_PIN_RESET);
+        HAL_Delay(1000);
+    }
 }
